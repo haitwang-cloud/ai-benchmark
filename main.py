14ee4240d5709dcbc21cf2cf551ec246eb854ce8
haitwang-cloud <haitao_wht@outlook.com>
2024-05-27 09:38:23 +0200

    feat: Add sys.exit(0) to main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 
 import ai_benchmark
+import sys
 
 
 def main():
     benchmark = ai_benchmark.AIBenchmark()
     results = benchmark.run_training(precision="high")
+    print(results)
+    sys.exit(0)
 
 
 
